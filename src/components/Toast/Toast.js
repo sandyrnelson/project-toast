1da69fc75e4e0cd314410566e89430d16dbffc40
Sandy Nelson <sandy.nelson@nexius.hu>
2025-01-07 09:17:58 +0100

    Refactor Toast component to take props for customization

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
-  AlertOctagon,
-  AlertTriangle,
-  CheckCircle,
-  Info,
-  X,
+	AlertOctagon,
+	AlertTriangle,
+	CheckCircle,
+	Info,
+	X,
 } from 'react-feather';
 
 import VisuallyHidden from '../VisuallyHidden';
@@ -12,27 +12,31 @@ import VisuallyHidden from '../VisuallyHidden';
 import styles from './Toast.module.css';
 
 const ICONS_BY_VARIANT = {
-  notice: Info,
-  warning: AlertTriangle,
-  success: CheckCircle,
-  error: AlertOctagon,
+	notice: Info,
+	warning: AlertTriangle,
+	success: CheckCircle,
+	error: AlertOctagon,
 };
 
-function Toast() {
-  return (
-    <div className={`${styles.toast} ${styles.notice}`}>
-      <div className={styles.iconContainer}>
-        <Info size={24} />
-      </div>
-      <p className={styles.content}>
-        16 photos have been uploaded
-      </p>
-      <button className={styles.closeButton}>
-        <X size={24} />
-        <VisuallyHidden>Dismiss message</VisuallyHidden>
-      </button>
-    </div>
-  );
+function Toast({ id, content, type, setToasts }) {
+	const IconTag = ICONS_BY_VARIANT[type];
+
+	return (
+		<div className={`${styles.toast} ${styles[type]}`}>
+			<div className={styles.iconContainer}>
+				<IconTag size={24} />
+			</div>
+			<p className={styles.content}>{content}</p>
+			<button
+				className={styles.closeButton}
+				onClick={() => {
+					setToasts((current) => current.filter((toast) => toast.id !== id));
+				}}>
+				<X size={24} />
+				<VisuallyHidden>Dismiss message</VisuallyHidden>
+			</button>
+		</div>
+	);
 }
 
 export default Toast;
