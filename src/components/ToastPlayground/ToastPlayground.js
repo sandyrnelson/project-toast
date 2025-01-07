0156a6a5a5c3a4bb54b072f1248e93bd852bf994
Sandy Nelson <sandy.nelson@nexius.hu>
2025-01-07 09:19:09 +0100

    Add toasts to playground for visibility

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -4,12 +4,20 @@ import Button from '../Button';
 import RadioButton from '../RadioButton';
 import TextArea from '../TextArea';
 import styles from './ToastPlayground.module.css';
+import ToastShelf from '../ToastShelf/ToastShelf';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
 	const [type, setType] = useState('notice');
 	const [message, setMessage] = React.useState('');
+	const [toasts, setToasts] = React.useState([]);
+
+	function createToast() {
+		const id = crypto.randomUUID();
+		setToasts([...toasts, { id: id, content: message, type: type }]);
+		setMessage('');
+	}
 
 	return (
 		<div className={styles.wrapper}>
@@ -17,7 +25,7 @@ function ToastPlayground() {
 				<img alt='Cute toast mascot' src='/toast.png' />
 				<h1>Toast Playground</h1>
 			</header>
-
+			<ToastShelf toasts={toasts} setToasts={setToasts} />
 			<div className={styles.controlsWrapper}>
 				<div className={styles.row}>
 					<TextArea
@@ -26,15 +34,6 @@ function ToastPlayground() {
 						value={message}
 						setter={setMessage}
 					/>
-					{/* <label
-						htmlFor='message'
-						className={styles.label}
-						style={{ alignSelf: 'baseline' }}>
-						Message
-					</label>
-					<div className={styles.inputWrapper}>
-						<textarea id='message' className={styles.messageInput} />
-					</div> */}
 				</div>
 				<div className={styles.row}>
 					<RadioButton
@@ -49,7 +48,12 @@ function ToastPlayground() {
 				<div className={styles.row}>
 					<div className={styles.label} />
 					<div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-						<Button>Pop Toast!</Button>
+						<Button
+							onClick={() => {
+								createToast();
+							}}>
+							Pop Toast!
+						</Button>
 					</div>
 				</div>
 			</div>
