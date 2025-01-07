345eec693608bf34aee54b9bf38f8beab9d35b86
Sandy Nelson <sandy.nelson@nexius.hu>
2025-01-07 06:53:24 +0100

    Create RadioButton component

---
+++ src/components/RadioButton/RadioButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './RadioButton.module.css';
+
+function RadioButton({ name, options, label = '', selected, setter }) {
+	return (
+		<>
+			{label !== '' && <div className={styles.label}>{label}</div>}
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+				}}>
+				<div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+					{options.map((option) => {
+						return (
+							<label key={option} htmlFor={`${name}-${option}`}>
+								<input
+									id={`${name}-${option}`}
+									type='radio'
+									name={name}
+									value={selected === option}
+									onChange={(event) => {
+										setter(event.target.value);
+									}}
+								/>
+								{option}
+							</label>
+						);
+					})}
+				</div>
+			</form>
+		</>
+	);
+}
+
+export default RadioButton;
